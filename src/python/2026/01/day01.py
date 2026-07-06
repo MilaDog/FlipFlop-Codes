38810f9045640981363fa02ed7811fbb22f56d8e
MilaDog <daniel.ryan.sergeant@gmail.com>
2026-07-06 14:27:54 +0200

    2026/01: some refactoring

--- src/python/2026/01/day01.py
+++ src/python/2026/01/day01.py
@@ -12,20 +12,36 @@ class Solution:
 
         return cls(data=values)
 
+    @staticmethod
+    def calculate_heating_cooling_time(current_temp: int, target_temp: int, perform_cooling: bool = False) -> int:
+        """Perform heating or cooling of the coffee to determine the time needed to get the original coffee to the
+        desired temperature.
+
+        Args:
+            current_temp (int): Current temperature of the coffee.
+            target_temp (int): Target temperature of the coffee.
+            perform_cooling (bool): Whether to perform cooling. Default is FALSE.
+
+        Returns:
+            int: Time take to get the original coffee to the desired temperature.
+        """
+        if not perform_cooling:
+            return max(target_temp - current_temp, 0)
+
+        diff: int = target_temp - current_temp
+        return diff if diff > 0 else abs(diff) * 5
+
     def part01(self) -> None:
         """Solution to Part 01."""
-        tlt: int = sum(max(60 - line, 0) for line in self.data)
-
+        tlt: int = sum(Solution.calculate_heating_cooling_time(current_temp=line, target_temp=60) for line in self.data)
         print(f"Part 01: {tlt}")
 
     def part02(self) -> None:
         """Solution to Part 02."""
-        tlt: int = 0
-
-        for line in self.data:
-            diff: int = 60 - line
-            tlt += diff if diff > 0 else abs(diff) * 5
-
+        tlt: int = sum(
+            Solution.calculate_heating_cooling_time(current_temp=line, target_temp=60, perform_cooling=True)
+            for line in self.data
+        )
         print(f"Part 02: {tlt}")
 
     def part03(self) -> None:
@@ -35,12 +51,10 @@ class Solution:
         coffee_temps: list[int] = self.data[:lngth]
         preferred_temps: list[int] = self.data[lngth:]
 
-        tlt: int = 0
-
-        for t1, t2 in zip(coffee_temps, preferred_temps):
-            diff: int = t2 - t1
-            tlt += diff if diff > 0 else abs(diff) * 5
-
+        tlt: int = sum(
+            Solution.calculate_heating_cooling_time(current_temp=t1, target_temp=t2, perform_cooling=True)
+            for t1, t2 in zip(coffee_temps, preferred_temps)
+        )
         print(f"Part 03: {tlt}")
 
 
